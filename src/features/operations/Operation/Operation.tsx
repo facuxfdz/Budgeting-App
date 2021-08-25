6a27af597079c9866f4e706649579a4ac153b416
Facundo Agustín Fernández <facuassain@gmail.com>
2021-08-25 00:07:21 -0300

    Operation options Modal added
    react-bootstrap installed to use a Modal component that will allow user to delete and edit any operation

--- src/features/operations/Operation/Operation.tsx
+++ src/features/operations/Operation/Operation.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import styles from './styles.module.scss'
+import Modal from 'react-bootstrap/Modal'
 
 interface OperationType {
     id: string,
@@ -21,6 +22,15 @@ const Operation: React.FC<OperationObject> = ({operation}) => {
 
     const {concept,amount} = operation
     
+    const handleDelete = () => {
+        alert('delete')
+        setShowOperationActions(false)
+    }
+
+    const handleEdit = () => {
+        alert('edit')
+        setShowOperationActions(false)
+    }
 
     return (
         <>
@@ -32,6 +42,34 @@ const Operation: React.FC<OperationObject> = ({operation}) => {
             <span className={`${styles.amount}`}>${amount}</span>
                 
         </div>
+        
+        <Modal
+            size="sm"
+            show={showOperationActions}
+            onHide={() => setShowOperationActions(false)}
+            aria-labelledby="operation-options"
+            centered
+        >
+            <Modal.Header>
+                <Modal.Title id="operation-options">
+                    Options
+                </Modal.Title>
+            </Modal.Header>
+            
+            <Modal.Body>What do you want to do?</Modal.Body>
+
+            <Modal.Footer>
+                <button 
+                    className="btn btn-danger" 
+                    onClick={handleDelete}
+                >Delete</button>
+
+                <button
+                    className="btn btn-warning"
+                    onClick={handleEdit}
+                >Edit</button>
+            </Modal.Footer>
+        </Modal>
 
         </>
     );
