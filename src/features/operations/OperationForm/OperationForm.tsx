7d260a0eea76393a91e2ed9bb65d8a7c39f675ff
Facundo Agustín Fernández <facuassain@gmail.com>
2021-08-26 18:56:55 -0300

    Operation add form formatted
    Now the form have all the necessary inputs to ask for the data we need. Local state added by using useState hook. Logic to save the operation not added yet

--- src/features/operations/OperationForm/OperationForm.tsx
+++ src/features/operations/OperationForm/OperationForm.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from './styles.module.scss'
 
 export interface IOperationFormProps {
     setShowOperationForm: React.Dispatch<React.SetStateAction<boolean>>
@@ -6,30 +7,75 @@ export interface IOperationFormProps {
 
 const OperationForm: React.FC<IOperationFormProps> = ({setShowOperationForm}) => {
 
+
+    // Logic to extract the date in the required format for the input
+    // By default, the operation will be the same day we are writting it
+    let date = new Date().toISOString()
+    const dateFormat = /\d\d\d\d-\d\d-\d\d/
+    const matchString = date.match(dateFormat)
+    date = matchString ? matchString[0] : ''
+
+    // Local state
+    const [concept,setConcept] = useState('')
+    const [amount,setAmount] = useState(0)
+    const [operationDate,setOperationDate] = useState(date)
+
+    
+    // Handle functions
     const handleCancel = () => {
         setShowOperationForm(false)
+        setConcept('')
+        setAmount(0)
+        setOperationDate('')
     }
-    
+
+    const handleSave = (e: React.FormEvent) => {
+        e.preventDefault()
+
+        // Save logic
+    }
+
     return (
-        <form>
+        <form className={styles.operationForm}>
         
-            <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+            <div className="mb-3 mt-3 text-warning">
+                <label htmlFor="concept" className="form-label">Concept</label>
+                <input 
+                    type="text" 
+                    value={concept} 
+                    onChange={e => setConcept(e.target.value)} 
+                    className="form-control" id="concept" 
+                />
             </div>
         
-            <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" />
+            <label htmlFor="amount" className="text-warning form-label">Amount</label>
+            <div className="mb-3 input-group">
+                <span className="input-group-text">$</span>
+                <input 
+                    type="number" 
+                    value={`${amount}`} 
+                    onChange={e => setAmount(Number(e.target.value) || 0)} 
+                    className="form-control" id="amount" 
+                />
             </div>
 
-            <div className="mb-3 form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+            <div className="mb-3 mt-3 text-warning">
+                <label htmlFor="date" className="form-label">Date</label>
+                <input 
+                    type="date" 
+                    defaultValue={operationDate} 
+                    onChange={e => setOperationDate(e.target.value)}
+                    className="form-control" id="date" 
+                />
             </div>
+
+
+            <button 
+                type="submit" 
+                className="btn btn-warning me-4"
+                onClick={handleSave}
+            >Save Operation</button>
             
-            <button type="submit" className="btn btn-primary me-4">Submit Operation</button>
             <button 
                 className="btn btn-danger"
                 onClick={handleCancel}    
