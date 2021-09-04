a0f662ac24a54057f7c11178e5b9718fcfb1b8e9
Facundo Agustín Fernández <facuassain@gmail.com>
2021-09-03 22:30:14 -0300

    Pagination added
    Logic to paginate operations added

--- src/app/components/index.tsx
+++ src/app/components/index.tsx
@@ -1,11 +1,7 @@
 import Loader from './Loader/Loader'
 import Navbar from './Navbar/Navbar'
-import NextPageButton from './NextPageButton/NextPageButton'
-import PrevPageButton from './PrevPageButton/PrevPageButton'
 
 export { 
     Loader, 
     Navbar, 
-    NextPageButton, 
-    PrevPageButton
 }
--- src/features/operations/OperationsList/OperationsList.tsx
+++ src/features/operations/OperationsList/OperationsList.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useAppSelector } from '../../../app/hooks';
 import { Operation } from '../'
 import { selectOperationByType, selectOperationByUser, selectUserBalance} from  '../operationsSlice'
@@ -7,15 +7,34 @@ import { calculateBalanceClass } from '../../../app/helpers'
 
 const OperationsList: React.FC = () => {
 
+    const DATALIMIT = 10
+
+    const [currentPage,setCurrentPage] = useState(1)
+    
+    
     const userEmail = useAppSelector(state => selectCurrentUserEmail(state))
     const userOperations = useAppSelector(state => selectOperationByUser(state,userEmail))
     const userBalance = useAppSelector(state => selectUserBalance(state))
-   
     const incomeOperations = useAppSelector(state => selectOperationByType(state,'income'))
     const expenseOperations = useAppSelector(state => selectOperationByType(state,'expense'))
     
     let balanceStatusClass = calculateBalanceClass(incomeOperations,expenseOperations)
-  
+    const pages = Math.ceil(userOperations.length / DATALIMIT)
+
+    const goToNextPage = () => {
+        setCurrentPage(page => page + 1)
+    }
+
+    const goToPreviousPage = () => {
+        setCurrentPage(page => page - 1)
+    }
+    
+    const getPaginatedData = () => {
+        const startIndex = currentPage * DATALIMIT - DATALIMIT
+        const endIndex = startIndex + DATALIMIT
+        return userOperations.slice(startIndex, endIndex)
+    }
+
 
     return (    
         <section className="container w-75 mt-5">
@@ -26,12 +45,37 @@ const OperationsList: React.FC = () => {
                     </div>
                 : null
             }
-                {userOperations.map(operation => (
-                    <Operation 
-                        key={operation.id}
-                        operation={operation}
-                    />
-                ))}
+
+            {userOperations.length > 0
+                ? 
+                (
+                    <>
+                        {getPaginatedData().map(operation => (
+                            <Operation 
+                                key={operation.id}
+                                operation={operation}
+                            />
+                        ))}
+
+                        <div className="text-center p-3">
+                        <button
+                            onClick={goToPreviousPage}
+                            disabled={currentPage === 1}
+                            className="me-2 btn btn-secondary"
+                        >prev</button>
+
+                        <button
+                            onClick={goToNextPage}
+                            disabled={currentPage === pages}
+                            className="ms-2 btn btn-secondary"
+                        >next</button>
+                        </div>
+
+                    </>
+                )
+                : null
+            }
+
         </section>
     );
 }
